58d1614ea6c12eb2f12b93827683d8e204c6f7bd
Paul Elion <paul.j.elion@gmail.com>
2018-05-22 11:40:29 -0400

    add file structure and JS processing

---
+++ scripts/game-es6.js
@@ -0,0 +1,14 @@
+// logic for count game
+
+class Game {
+	constructor() {
+		console.log('Welcome to the game. Version ${this.version()})';
+	}
+	version(){
+		return "1.0.0"
+	}
+}
+
+// start the game
+
+var game = new Game();
